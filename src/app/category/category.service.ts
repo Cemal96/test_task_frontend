86fe6e14240f2754da34c92e104bbd6006e0d7e8
Maxym Bulgar <maksbulgar@gmail.com>
2016-11-06 02:53:27 +0200

    Add Category service to api

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,4 +1,7 @@
 import { Injectable }    from '@angular/core';
+import { Http, Response} from '@angular/http';
+import { URLSearchParams } from '@angular/http';
+import { Observable } from "rxjs/Rx";
 
 import { Category } from './category';
 
@@ -7,9 +10,24 @@ const CATEGORIES: Category[] = [
   {id: 2, name: 'Ultrabook'},
   {id: 3, name: 'Desktop'}
 ];
+
 @Injectable()
 export class CategoryService {
-  getCategories(): Category[] {
-    return CATEGORIES;
+
+  constructor(private _http: Http) { }
+
+  private apiUrl = 'http://localhost:3001/';
+
+  getVehicles() {
+    return this._http.get(this.apiUrl + 'categories.json')
+      .map((response: Response) => <Category[]>response.json())
+      .toPromise()
+      .catch(this.handleError);
   }
+
+  private handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
+
 }
